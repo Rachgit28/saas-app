8b434c498066a5b55fdfb2897fe82de5cdb636d0
Rachit Rajput <28rachitrajput@gmail.com>
2025-06-06 10:02:51 +0530

    Updated files

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,32 @@ const Page = () => {
     <main>
       <h1 className="text-2xl underline">Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard />
-        <CompanionCard />
-        <CompanionCard />
+        <CompanionCard 
+        id="123"
+        name="Neuro the Brainy Explorer"
+        topic="Neural Network of the Brain"
+        subject="science"
+        duration={45}
+        color="#ffda6e"
+        />
+
+        <CompanionCard 
+        id="456"
+        name="Countsy the Number Wizard"
+        topic="Derivatives & Integrals"
+        subject="maths"
+        duration={30}
+        color="#e5d0ff"
+        />
+        
+        <CompanionCard 
+        id="789"
+        name="Verba the Vocabulary Builder"
+        topic="English Literature"
+        subject="language"
+        duration={30}
+        color="#BDE7FF"
+        />
       </section>
 
       <section className="home-section">
