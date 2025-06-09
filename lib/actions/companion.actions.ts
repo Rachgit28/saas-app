effb605d31247f77775f9f31be55d6ea4701c44f
Rachit Rajput <28rachitrajput@gmail.com>
2025-06-09 10:44:37 +0530

    Updated files

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,7 +1,6 @@
 'use server';
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase";
-import { TrendingUpDown } from "lucide-react";
 import { revalidatePath } from "next/cache";
 
 export const createCompanion = async(formData: CreateCompanion) => {
