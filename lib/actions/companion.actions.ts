1352ae549291bd28bd218a8a58b48f6637f2fa51
Rachit Rajput <28rachitrajput@gmail.com>
2025-06-09 09:59:02 +0530

    Updated files

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -1,6 +1,7 @@
 'use server';
 import { auth } from "@clerk/nextjs/server"
 import { createSupabaseClient } from "../supabase";
+import { TrendingUpDown } from "lucide-react";
 
 export const createCompanion = async(formData: CreateCompanion) => {
     const {userId: author} = await auth();
@@ -71,3 +72,28 @@ export const getUserCompanions = async(userId:string) => {
     if(error) throw new Error(error.message);
     return data;
 }
+
+export const newCompanionPermissions = async() => {
+    const {userId, has } = await auth();
+    const supabase= createSupabaseClient();
+    let limit = 0;
+
+    if(has({plan:'pro'})) {
+        return true;
+    } else if(has({feature: "3_companion_limit"})) {
+        limit =3;
+    } else if(has({feature: "10_companion_limit"})) {
+        limit =10;
+    }
+
+    const {data, error} = await supabase.from('companions').select('id',{count:'exact'}).eq('author',userId)
+    if(error) throw new Error(error.message);
+
+    const companionCount = data?.length;
+    if(companionCount > limit) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
