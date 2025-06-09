d0503779eb688a949c0e1f0d229bebb71f263ed7
Rachit Rajput <28rachitrajput@gmail.com>
2025-06-09 10:23:07 +0530

    Updated files

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -76,24 +76,19 @@ export const getUserCompanions = async(userId:string) => {
 export const newCompanionPermissions = async() => {
     const {userId, has } = await auth();
     const supabase= createSupabaseClient();
-    let limit = 0;
-
+ 
     if(has({plan:'pro'})) {
         return true;
-    } else if(has({feature: "3_companion_limit"})) {
-        limit =3;
-    } else if(has({feature: "10_companion_limit"})) {
-        limit =10;
-    }
+    } 
 
-    const {data, error} = await supabase.from('companions').select('id',{count:'exact'}).eq('author',userId)
+    let limit = 3; 
+  if (has({ plan: 'core' })) {
+    limit = 10;
+  }
+
+    const {data, error, count} = await supabase.from('companions').select('id',{count:'exact', head:true}).eq('author',userId)
     if(error) throw new Error(error.message);
 
-    const companionCount = data?.length;
-    if(companionCount > limit) {
-        return false;
-    } else {
-        return true;
-    }
+    return (count ?? 0) < limit;
 }
 
